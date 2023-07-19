19c8fbcb99004f4679d9e993ae1ca60b52233719
wangchen <wangchen41@xiaomi.com>
2023-07-19 10:19:37 +0800

    net:Resolve udp disconnection, status not synchronized error
    VELAPLATFO-11484
    
    Change-Id: I513b2525ececccbf8828092b3b8e8242988e8921
    Signed-off-by: wangchen <wangchen41@xiaomi.com>

--- net/udp/udp_conn.c
+++ net/udp/udp_conn.c
@@ -957,6 +957,8 @@ int udp_connect(FAR struct udp_conn_s *conn, FAR const struct sockaddr *addr)
 
   if (addr != NULL)
     {
+      conn->sconn.s_flags |= _SF_CONNECTED;
+
 #ifdef CONFIG_NET_IPv4
 #ifdef CONFIG_NET_IPv6
       if (conn->domain == PF_INET)
@@ -1003,6 +1005,7 @@ int udp_connect(FAR struct udp_conn_s *conn, FAR const struct sockaddr *addr)
   else
     {
       conn->rport = 0;
+      conn->sconn.s_flags &= ~_SF_CONNECTED;
 
 #ifdef CONFIG_NET_IPv4
 #ifdef CONFIG_NET_IPv6
