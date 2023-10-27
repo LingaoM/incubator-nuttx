57299ecec8db172ce41df8d3d2ba7b0b76189f9c
guohao15 <guohao15@xiaomi.com>
2023-10-27 15:29:41 +0800

    api:add lib_realpath function
    VELAPLATFO-16331
    
    FAR char *lib_realpath(FAR const char *path, FAR char *resolved, bool notfollow);
    
    Change-Id: I7d4f5d65148cd17cf082362336ef55a9393f4232
    Signed-off-by: guohao15 <guohao15@xiaomi.com>

--- include/nuttx/lib/lib.h
+++ include/nuttx/lib/lib.h
@@ -122,6 +122,11 @@ FAR struct file_struct *lib_get_stream(int fd);
 
 unsigned long nrand(unsigned long limit);
 
+/* Functions defined in lib_realpath.c **************************************/
+
+FAR char *lib_realpath(FAR const char *path, FAR char *resolved,
+                       bool notfollow);
+
 #undef EXTERN
 #ifdef __cplusplus
 }
