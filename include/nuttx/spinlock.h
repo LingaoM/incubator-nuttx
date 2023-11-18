23374d2dd62068fddcc0dd7f970121b8aacc60b1
Xiang Xiao <xiaoxiang@xiaomi.com>
2023-11-18 20:10:27 +0800

    spinlock: Fix misc/goldfish_pipe.c:643:3: error: implicit declaration of function 'spin_lock_init'
    VELAPLATFO-30865
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>
    (cherry picked from commit cb81306377f6fb6ac5150f153ef75c88fb09e7a9)

--- include/nuttx/spinlock.h
+++ include/nuttx/spinlock.h
@@ -37,9 +37,7 @@
 #  define SP_LOCKED   1  /* The Locked state */
 
 typedef uint8_t spinlock_t;
-#else
-
-#ifdef CONFIG_TICKET_SPINLOCK
+#elif defined(CONFIG_TICKET_SPINLOCK)
 
 union spinlock_u
 {
@@ -69,7 +67,7 @@ typedef union spinlock_u spinlock_t;
 
 #include <arch/spinlock.h>
 
-#endif
+#endif /* CONFIG_SPINLOCK */
 
 /****************************************************************************
  * Pre-processor Definitions
@@ -375,8 +373,6 @@ void spin_clrbit(FAR volatile cpu_set_t *set, unsigned int cpu,
                  FAR volatile spinlock_t *setlock,
                  FAR volatile spinlock_t *orlock);
 #endif
-
-#endif /* CONFIG_SPINLOCK */
 
 /****************************************************************************
  * Name: spin_initialize
