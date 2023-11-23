22c2c507584af2bbca8f1310ec05f38115f5db40
Shoukui Zhang <zhangshoukui@xiaomi.com>
2023-11-23 21:06:50 +0800

    add setup and shutdown for i2c master and slave
    VELAPLATFO-21652
    
    Change-Id: Ia8a4310e08f7628fe3be7662311f329f9a1c5135
    Signed-off-by: Shoukui Zhang <zhangshoukui@xiaomi.com>

--- drivers/i2c/i2c_driver.c
+++ drivers/i2c/i2c_driver.c
@@ -137,13 +137,25 @@ static int i2cdrvr_open(FAR struct file *filep)
       return ret;
     }
 
+  /* I2c master initialize */
+
+  if (priv->i2c->ops->setup != NULL && priv->crefs == 0)
+    {
+      ret = I2C_SETUP(priv->i2c);
+      if (ret < 0)
+        {
+          goto out;
+        }
+    }
+
   /* Increment the count of open references on the driver */
 
   priv->crefs++;
   DEBUGASSERT(priv->crefs > 0);
 
+out:
   nxmutex_unlock(&priv->lock);
-  return OK;
+  return ret;
 }
 #endif
 
@@ -173,6 +185,17 @@ static int i2cdrvr_close(FAR struct file *filep)
       return ret;
     }
 
+  /* I2c master uninitialize */
+
+  if (priv->i2c->ops->shutdown != NULL && priv->crefs == 1)
+    {
+      ret = I2C_SHUTDOWN(priv->i2c);
+      if (ret < 0)
+        {
+          goto out;
+        }
+    }
+
   /* Decrement the count of open references on the driver */
 
   DEBUGASSERT(priv->crefs > 0);
@@ -189,8 +212,9 @@ static int i2cdrvr_close(FAR struct file *filep)
       return OK;
     }
 
+out:
   nxmutex_unlock(&priv->lock);
-  return OK;
+  return ret;
 }
 #endif
 
