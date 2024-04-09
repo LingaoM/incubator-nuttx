ca97307e5b96674e6171bb92acdcbe4167fc9414
ligd <liguiding1@xiaomi.com>
2024-04-09 18:32:53 +0800

    mps: update the mps3-an547 mps2-an500 defconfig
    EQEELIN-1
    
    Fix compile failed when open BASEPRI
    Open same feature on mps3
    
    Change-Id: I0e857ac8e621a4761ba9b4022376fd43dc4abc89
    Signed-off-by: ligd <liguiding1@xiaomi.com>

--- arch/arm/src/mps/mps_irq.c
+++ arch/arm/src/mps/mps_irq.c
@@ -85,7 +85,6 @@ static int mps_reserved(int irq, void *context, void *arg)
  *
  ****************************************************************************/
 
-#ifdef CONFIG_ARMV8M_USEBASEPRI
 static inline void mps_prioritize_syscall(int priority)
 {
   uint32_t regval;
@@ -97,7 +96,6 @@ static inline void mps_prioritize_syscall(int priority)
   regval |= (priority << NVIC_SYSH_PRIORITY_PR11_SHIFT);
   putreg32(regval, NVIC_SYSH8_11_PRIORITY);
 }
-#endif
 
 /****************************************************************************
  * Public Functions
@@ -260,14 +258,9 @@ void up_irqinitialize(void)
 
   /* Set the priority of the SVCall interrupt */
 
-#ifdef CONFIG_ARCH_IRQPRIO
+  up_prioritize_irq(MPS_IRQ_PENDSV, NVIC_SYSH_PRIORITY_MIN);
 
-  /* up_prioritize_irq(MPS_IRQ_PENDSV, NVIC_SYSH_PRIORITY_MIN); */
-
-#endif
-#ifdef CONFIG_ARMV8M_USEBASEPRI
   mps_prioritize_syscall(NVIC_SYSH_SVCALL_PRIORITY);
-#endif
 
   /* If the MPU is enabled, then attach and enable the Memory Management
    * Fault handler.
