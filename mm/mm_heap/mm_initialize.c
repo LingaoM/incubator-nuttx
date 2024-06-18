efc4b4e3b38579755b6863987eb9c9c3bc7fddd1
buxiasen <buxiasen@xiaomi.com>
2024-06-18 14:56:16 +0800

    mm: add mm_initialize_pool, make pool more flexible
    VELAPLATFO-35160
    
    Change-Id: I3f6d7da3ffbc6064b281bf76d6eef3625dc13b8c
    Signed-off-by: buxiasen <buxiasen@xiaomi.com>

--- mm/mm_heap/mm_initialize.c
+++ mm/mm_heap/mm_initialize.c
@@ -91,7 +91,7 @@ static void mm_dump_handler(FAR struct tcb_s *tcb, FAR void *arg)
 }
 #  endif
 
-#  if CONFIG_MM_HEAP_MEMPOOL_THRESHOLD != 0
+#  ifdef CONFIG_MM_HEAP_MEMPOOL
 static void mm_mempool_dump_handle(FAR struct mempool_s *pool, FAR void *arg)
 {
   struct mempoolinfo_s info;
@@ -277,7 +277,6 @@ void mm_addregion(FAR struct mm_heap_s *heap, FAR void *heapstart,
  *
  * Input Parameters:
  *   name      - The heap procfs name
- *   heap      - The selected heap
  *   heapstart - Start of the initial heap region
  *   heapsize  - Size of the initial heap region
  *
@@ -291,9 +290,6 @@ void mm_addregion(FAR struct mm_heap_s *heap, FAR void *heapstart,
 FAR struct mm_heap_s *mm_initialize(FAR const char *name,
                                     FAR void *heapstart, size_t heapsize)
 {
-#if CONFIG_MM_HEAP_MEMPOOL_THRESHOLD > 0
-  size_t poolsize[MEMPOOL_NPOOLS];
-#endif
   FAR struct mm_heap_s *heap;
   uintptr_t             heap_adj;
   int                   i;
@@ -352,24 +348,6 @@ FAR struct mm_heap_s *mm_initialize(FAR const char *name,
 #  endif
 #endif
 
-  /* Initialize the multiple mempool in heap */
-
-#if CONFIG_MM_HEAP_MEMPOOL_THRESHOLD > 0
-  for (i = 0; i < MEMPOOL_NPOOLS; i++)
-    {
-      poolsize[i] = (i + 1) * MM_MIN_CHUNK;
-    }
-
-  heap->mm_threshold = CONFIG_MM_HEAP_MEMPOOL_THRESHOLD;
-  heap->mm_mpool     = mempool_multiple_init(name, poolsize, MEMPOOL_NPOOLS,
-                              (mempool_multiple_alloc_t)mempool_memalign,
-                              (mempool_multiple_alloc_size_t)mm_malloc_size,
-                              (mempool_multiple_free_t)mm_free, heap,
-                              CONFIG_MM_HEAP_MEMPOOL_CHUNK_SIZE,
-                              CONFIG_MM_HEAP_MEMPOOL_EXPAND_SIZE,
-                              CONFIG_MM_HEAP_MEMPOOL_DICTIONARY_EXPAND_SIZE);
-#endif
-
 #ifdef CONFIG_MM_PANIC_ON_FAILURE
   heap->nb.notifier_call = mm_panic_dump;
   heap->oom = false;
@@ -378,6 +356,60 @@ FAR struct mm_heap_s *mm_initialize(FAR const char *name,
 
   return heap;
 }
+
+#ifdef CONFIG_MM_HEAP_MEMPOOL
+FAR struct mm_heap_s *
+mm_initialize_pool(FAR const char *name,
+                   FAR void *heap_start, size_t heap_size,
+                   FAR const struct mempool_init_s *init)
+{
+  FAR struct mm_heap_s *heap;
+
+#if CONFIG_MM_HEAP_MEMPOOL_THRESHOLD > 0
+  size_t poolsize[MEMPOOL_NPOOLS];
+  struct mempool_init_s def;
+
+  if (init == NULL)
+    {
+      /* Initialize the multiple mempool default parameter */
+
+      int i;
+
+      for (i = 0; i < MEMPOOL_NPOOLS; i++)
+        {
+          poolsize[i] = (i + 1) * MM_MIN_CHUNK;
+        }
+
+      def.poolsize        = poolsize;
+      def.npools          = MEMPOOL_NPOOLS;
+      def.threshold       = CONFIG_MM_HEAP_MEMPOOL_THRESHOLD;
+      def.chunksize       = CONFIG_MM_HEAP_MEMPOOL_CHUNK_SIZE;
+      def.expandsize      = CONFIG_MM_HEAP_MEMPOOL_EXPAND_SIZE;
+      def.dict_expendsize = CONFIG_MM_HEAP_MEMPOOL_DICTIONARY_EXPAND_SIZE;
+
+      init = &def;
+    }
+#endif
+
+  heap = mm_initialize(name, heap_start, heap_size);
+
+  /* Initialize the multiple mempool in heap */
+
+  if (init != NULL && init->poolsize != NULL && init->npools != 0)
+    {
+      heap->mm_threshold = CONFIG_MM_HEAP_MEMPOOL_THRESHOLD;
+      heap->mm_mpool     = mempool_multiple_init(name, init->poolsize,
+                               init->npools,
+                               (mempool_multiple_alloc_t)mempool_memalign,
+                               (mempool_multiple_alloc_size_t)mm_malloc_size,
+                               (mempool_multiple_free_t)mm_free, heap,
+                               init->chunksize, init->expandsize,
+                               init->dict_expendsize);
+    }
+
+  return heap;
+}
+#endif
 
 /****************************************************************************
  * Name: mm_uninitialize
