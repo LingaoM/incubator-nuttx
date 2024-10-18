e0c65f1fddf892ec02fe7dd1e18cbc88b2569945
zhangshoukui <zhangshoukui@xiaomi.com>
2024-10-18 15:34:36 +0800

    bmi160: fix Parameter passing error when use spi
    VELAPLATFO-45270
    
    nuttx/drivers/sensors/bmi160_uorb.c:596:18: warning: passing argument 1 of ‘bmi160_getreg8’ from incompatible pointer type [-Wincompatible-pointer-types]
      596 |   bmi160_getreg8(priv, 0x7f);
          |                  ^~~~
          |                  |
          |                  struct bmi160_dev_uorb_s *
    In file included from /workspace1/vela/sim/nuttx/drivers/sensors/bmi160_uorb.c:25:
    nuttx/drivers/sensors/bmi160_base.h:235:49: note: expected ‘struct bmi160_dev_s *’ but argument is of type ‘struct bmi160_dev_uorb_s *’
      235 | uint8_t bmi160_getreg8(FAR struct bmi160_dev_s *priv, uint8_t regaddr);
          |                            ~~~~~~~~~~~~~~~~~~~~~^~~~
    /workspace1/vela/sim/nuttx/drivers/sensors/bmi160_uorb.c:597:18: warning: passing argument 1 of ‘bmi160_getreg8’ from incompatible pointer type [-Wincompatible-pointer-types]
      597 |   bmi160_getreg8(priv, 0x7f); /* workaround: fail to switch SPI, run twice */
          |                  ^~~~
          |                  |
          |                  struct bmi160_dev_uorb_s *
    
    Change-Id: Ib5a9fb4c16349b863654175707a0499ddca01e88
    Signed-off-by: zhangshoukui <zhangshoukui@xiaomi.com>
    (cherry picked from commit 593cc56d62db43943f3a1b68d9900a6bab9be8fe)

--- drivers/sensors/bmi160_uorb.c
+++ drivers/sensors/bmi160_uorb.c
@@ -588,12 +588,12 @@ static int bmi160_register_accel(int devno,
   priv->dev.freq = BMI160_I2C_FREQ;
 
 #else /* CONFIG_SENSORS_BMI160_SPI */
-  priv->devl.spi = dev;
+  priv->dev.spi = dev;
 
   /* BMI160 detects communication bus is SPI by rising edge of CS. */
 
-  bmi160_getreg8(priv, 0x7f);
-  bmi160_getreg8(priv, 0x7f); /* workaround: fail to switch SPI, run twice */
+  bmi160_getreg8(&priv->dev, 0x7f);
+  bmi160_getreg8(&priv->dev, 0x7f); /* workaround: fail to switch SPI, run twice */
   usleep(200);
 
 #endif
