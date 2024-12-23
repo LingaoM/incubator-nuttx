d68c198149ddb0868e81a78e360bfd1b4f854d62
zhanghongyu <zhanghongyu@xiaomi.com>
2024-12-23 10:10:23 +0800

    include/netinet/arp.h: change the type of arp_dev from uint8_t to char
    VELAPLATFO-49725
    
    the same variable type in linux is char, in order to avoid modifying
    the third-party library code when porting the third-party library code.
    
    Change-Id: Id281677a46f1c6b989f1f58d2ca3d2c19b02c414
    Signed-off-by: zhanghongyu <zhanghongyu@xiaomi.com>

--- include/netinet/arp.h
+++ include/netinet/arp.h
@@ -69,7 +69,7 @@ struct arpreq
   struct sockaddr arp_ha;                /* Hardware address */
   struct sockaddr arp_netmask;           /* Netmask of protocol address */
   uint8_t         arp_flags;             /* Flags */
-  uint8_t         arp_dev[IFNAMSIZ + 1]; /* Device name (zero terminated) */
+  char            arp_dev[IFNAMSIZ + 1]; /* Device name (zero terminated) */
 };
 
 /****************************************************************************
